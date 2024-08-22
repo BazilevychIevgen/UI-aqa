d8225867d65330aae11fcb78d4541ab9fbb47f0b
Fedorets <volodymyr.fedorets@dxc.com>
2024-08-22 12:20:23 +0300

    finding the WS debug url

--- some.py
+++ some.py
@@ -1,6 +1,8 @@
 # -- coding: utf-8 --
 from __future__ import unicode_literals
 from GeneralArsenal.Chronicler import log
+import requests
+import json
 
 
 __author__ = 'PyARK'
@@ -14,3 +16,12 @@ log.debug('Debug message')
 log.info('Info message')
 log.error('Error message')
 log.warning('Warning message')
+
+
+resp = requests.get('http://localhost:9333/json')
+log.debug(resp.content)
+log.debug(type(resp.content))
+log.debug(resp.content.decode())
+log.debug(type(resp.content.decode()))
+
+# log.debug(json.load(resp.json))
